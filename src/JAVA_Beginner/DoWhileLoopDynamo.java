6f5499fc48609dbd08666834c53b2b3531d87369
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-07 22:40:24 +0530

    do while

---
+++ src/JAVA_Beginner/DoWhileLoopDynamo.java
@@ -0,0 +1,17 @@
+package JAVA_Beginner;
+
+public class DoWhileLoopDynamo {
+
+	public static void main(String[] args) {
+		int num =10;
+		do
+		{
+		
+			System.out.println(num);
+			num--;
+		}while(num>0);
+		
+		System.out.println();
+	}
+
+}
