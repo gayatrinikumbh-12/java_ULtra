d2b4071b3894cd0663fc8247bc4635f478beedc8
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-07 21:42:28 +0530

    var type

---
+++ src/JAVA_Beginner/DataTypeDetective.java
@@ -0,0 +1,19 @@
+package JAVA_Beginner;
+
+public class DataTypeDetective {
+
+	public static void main(String[] args) {
+		
+		int a =70;
+		double b=56.77;
+		char v = 't';
+		boolean n = true;
+		
+		System.out.println(a+" is a Integer");
+		System.out.println(b +" is a double");
+		System.out.println(v+" is a char");
+		System.out.println(n+" is a boolean");
+		
+	}
+
+}
