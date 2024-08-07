739cbecc09f1d1ba0dafb7f2afea5dc761400412
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-07 20:23:44 +0530

    righttype

---
+++ src/JAVA_Beginner/RightTypeRiddle.java
@@ -0,0 +1,21 @@
+package JAVA_Beginner;
+
+public class RightTypeRiddle {
+
+	public static void main(String[] args) {
+			int a;
+			char b;
+			double c;
+			boolean d;
+			
+			a= 100;
+			b= 'c';
+			c= 8.77;
+			d=false;
+			System.out.println("INTEGER "+a);
+			System.out.println("CHAR "+b);
+			System.out.println("DOUBLE "+c);
+			System.out.println("BOOLEAN "+d);
+	}
+
+}
