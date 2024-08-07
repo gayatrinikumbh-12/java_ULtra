f42425df3ac57112a0e37ea1067891a950a8b576
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-07 21:46:34 +0530

    convert

---
+++ src/JAVA_Beginner/GreatTypeTransformation.java
@@ -0,0 +1,13 @@
+package JAVA_Beginner;
+
+public class GreatTypeTransformation {
+
+	public static void main(String[] args) {
+
+		double b = 99.999;
+		int a ;
+		a= (int) b;
+		System.out.println("convert"+a);
+	}
+
+}
