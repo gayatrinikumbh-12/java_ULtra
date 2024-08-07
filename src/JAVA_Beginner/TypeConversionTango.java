5e4635b6456c85822f82a61f280d104e9aea80ee
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-07 18:30:25 +0530

    first program

---
+++ src/JAVA_Beginner/TypeConversionTango.java
@@ -0,0 +1,17 @@
+package JAVA_Beginner;
+
+public class TypeConversionTango {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		double a = 7.5;
+		int b = 5;
+		
+		int c = (int) a;
+		double d = b;
+		System.out.println("variables after converting after double and integer "+c+" and "+d);
+	}
+
+}
