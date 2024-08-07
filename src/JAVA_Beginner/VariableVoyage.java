cbffba68f00d27a8aa49ea3c384637a1af8aa274
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-07 20:27:28 +0530

    sum

---
+++ src/JAVA_Beginner/VariableVoyage.java
@@ -0,0 +1,16 @@
+package JAVA_Beginner;
+
+public class VariableVoyage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a ,b,sum;
+		a=340;
+		b=120;
+		sum = a+b;
+		System.out.println("addition"+sum);
+		
+	}
+
+}
