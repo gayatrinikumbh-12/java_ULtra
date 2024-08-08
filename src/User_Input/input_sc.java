6e2dd55d8b63f07de768ca35a311d5108bd8ce12
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-08 18:36:48 +0530

    msg

---
+++ src/User_Input/input_sc.java
@@ -0,0 +1,22 @@
+package User_Input;
+
+import java.util.Scanner;
+
+public class input_sc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		System.out.println("enter int ");
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt());
+		
+		System.out.println("enter double ");
+		System.out.println(sc.nextDouble());
+		
+		System.out.println("enter string ");
+		System.out.println(sc.nextLine());
+	}
+
+}
