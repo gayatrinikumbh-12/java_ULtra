e9b8e4c730ef9f419f00fba2e2f39003d69b0fa1
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-08 21:34:29 +0530

    h

---
+++ src/User_Input/Test.java
@@ -0,0 +1,30 @@
+package User_Input;
+
+public class Test {
+
+	
+	static int a ;
+	int b ;
+	
+
+	public void show()
+	{
+		int c;
+		c = 15;
+		System.out.println(c);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Test t = new Test();
+		t.a = 5;
+		t.b = 10;
+		t.show();
+		System.out.println("a "+a);
+		System.out.println("b "+t.b);
+		
+		
+	}
+
+}
