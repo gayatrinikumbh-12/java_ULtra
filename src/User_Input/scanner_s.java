70dfe64f418cd42d53d48da4931482eb590b626a
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-08 21:47:35 +0530

    h

---
+++ src/User_Input/scanner_s.java
@@ -0,0 +1,29 @@
+package User_Input;
+
+import java.util.Scanner;
+
+public class scanner_s {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Enter string");
+		
+		String a = sc.next();
+		
+		System.out.println("Entered string "+a);
+		
+		String[] b = a.split(" ");
+		
+		for(String f :b)
+		{
+			System.out.println(f);
+		}
+		
+		
+	}
+
+}
