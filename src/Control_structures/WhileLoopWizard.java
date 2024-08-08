998444405efdeec2056bfd5467655e1180e9b65e
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-08 17:28:18 +0530

    f

--- src/Control_structures/WhileLoopWizard.java
+++ src/Control_structures/WhileLoopWizard.java
@@ -1,27 +1,42 @@
 package Control_structures;
 
+import java.util.ArrayList;
+
 public class WhileLoopWizard {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int num1 , num2, next_num, n;
 		num1=0;
 		num2=1;
 		
 		n=1;
-		System.out.println(num1);
 		
-		System.out.println(num2);
+		ArrayList <Integer> a = new  <Integer> ArrayList();
+		
+		
+	
 		while(n<=8)
 		{
 			next_num= num1+num2;
-			System.out.println(next_num);
+			a.add(next_num);
+			//System.out.println(next_num);
 			n++;
 			num1=num2;
 			num2=next_num;
 			
 			
 		}
+		
+		//Object[] b_arr=a.toArray();
+		
+		for(int i = a.size()-1; i>0;i--)
+		{
+			System.out.println(a.get(i));
+		}
+		System.out.println(1);
+		System.out.println(0);
+		
 	}
 
 }
