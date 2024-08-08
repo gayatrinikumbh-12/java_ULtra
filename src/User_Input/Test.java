f091ec69bd5a188523146499b25fb87a87037299
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-08 23:00:01 +0530

    k

--- src/User_Input/Test.java
+++ src/User_Input/Test.java
@@ -2,30 +2,17 @@ package User_Input;
 
 public class Test {
 
+	static int x =7 ; 
 	
-	static int a ;
-	int b ;
-	
-
-	public int show(int i )
-	{
-		int c;
-		c = i;
-		//System.out.println(c);
-		return c;
-	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int d ;
-		
 		Test t = new Test();
-		t.a = 5;
-		t.b = 10;
-		d= t.show(8);
-		System.out.println("a "+a);
-		System.out.println("b "+t.b);
+		Test t1 = new Test();
 		
-		System.out.println("d "+d);
+		System.out.println("first "+t.x);
+		t.x = 9;
+		System.out.println("ssecond "+t.x);
+		System.out.println("ssecond "+t1.x);
 	}
 
 }
---
+++ src/User_Input/Test_p.java
@@ -0,0 +1,31 @@
+package User_Input;
+
+public class Test_p {
+
+	
+	static int a ;
+	int b ;
+	
+
+	public int show(int i )
+	{
+		int c;
+		c = i;
+		//System.out.println(c);
+		return c;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int d ;
+		
+		Test_p t = new Test_p();
+		t.a = 5;
+		t.b = 10;
+		d= t.show(8);
+		System.out.println("a "+a);
+		System.out.println("b "+t.b);
+		
+		System.out.println("d "+d);
+	}
+
+}
