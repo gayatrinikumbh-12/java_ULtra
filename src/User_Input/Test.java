9f6b2a9c7f5b2969969f6ef955e159b73ae67b44
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-08 21:39:00 +0530

    j

--- src/User_Input/Test.java
+++ src/User_Input/Test.java
@@ -7,24 +7,25 @@ public class Test {
 	int b ;
 	
 
-	public void show()
+	public int show(int i )
 	{
 		int c;
-		c = 15;
-		System.out.println(c);
+		c = i;
+		//System.out.println(c);
+		return c;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int d ;
 		
 		Test t = new Test();
 		t.a = 5;
 		t.b = 10;
-		t.show();
+		d= t.show(8);
 		System.out.println("a "+a);
 		System.out.println("b "+t.b);
 		
-		
+		System.out.println("d "+d);
 	}
 
 }
