8b73afe3ac05f8ca7b2c98a7616b43834d03fc6e
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-08 13:14:41 +0530

    m

---
+++ src/Control_structures/DoWhileLoopDynamo.java
@@ -0,0 +1,19 @@
+package Control_structures;
+
+public class DoWhileLoopDynamo {
+
+	public static void main(String[] args) {
+		
+		int num ;
+		num = 10;
+		do
+		{
+			
+			System.out.println(num);
+			num--;
+		}
+		while(num>0);
+		
+	}
+
+}
