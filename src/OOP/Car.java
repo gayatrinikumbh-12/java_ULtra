560623100c4d42786c9c27ef1bef25152235f9e5
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-09 20:35:07 +0530

    r

--- src/OOP/Car.java
+++ src/OOP/Car.java
@@ -3,11 +3,11 @@ package OOP;
 public class Car {
 
 	
-	String colour;
+	String color;
 	String model;
 	public Car(String c , String m)
 	{
-		this.colour = c;
+		this.color = c;
 		this.model = m;
 	}
 	
@@ -17,14 +17,14 @@ public class Car {
 	}
 	public void describe()
 	{
-		System.out.println("This is a "+ colour +" "+model);
+		System.out.println("This is a "+ color +" "+model);
 		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Car c = new Car("White", "Audi");
 		Car c1 = new Car();
-		c1.colour = "black";
+		c1.color = "black";
 		c1.model="Audi";
 		//System.out.println(c1.colour+" "+c1.model);
 		c.describe();
