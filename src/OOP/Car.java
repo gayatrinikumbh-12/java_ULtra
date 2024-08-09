ebde787020ba633ce6422a6f4fdbecf556fad39a
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-09 20:18:32 +0530

    f

--- src/OOP/Car.java
+++ src/OOP/Car.java
@@ -10,9 +10,17 @@ public class Car {
 		this.colour = c;
 		this.model = m;
 	}
+	public Car()
+	{
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Car c = new Car("White", "Audi");
+		Car c1 = new Car();
+		c1.colour = "black";
+		c1.model="Audi";
+		System.out.println(c1.colour+" "+c1.model);
 		
 	}
 
