2e0936f3be32f23c2d488fb5fb5ace2c8e392c7d
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-10 19:04:59 +0530

    r

--- src/OOP/Car.java
+++ src/OOP/Car.java
@@ -8,18 +8,30 @@ public class Car {
 	public double price;
 	
 	public void describe() {
-		System.out.println("color"+color);
+		
 		System.out.println("model"+model);
 		System.out.println("year"+year);
 		System.out.println("price"+price);
 	}
 	
-
+	public String get_color() {
+		System.out.println("color"+color);
+		return color;
+	}
+	
+	public String set_color(String clr) {
+		return this.color=clr;
+	}
+	
+	
+	
 	public static void main(String[] args) {
 
 		
 		Car c = new Car();
-		c.color = "white";
+		//c.color = "white";
+		c.set_color("black");
+		c.get_color();
 		c.model ="venue";
 		c.year= 2018;
 		c.price = 20.23;
