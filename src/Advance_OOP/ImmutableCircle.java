f02bcf149c9a8e2fdb49b6b9342782349491ffdf
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-13 23:20:23 +0530

    o

--- src/Advance_OOP/ImmutableCircle.java
+++ src/Advance_OOP/ImmutableCircle.java
@@ -2,13 +2,20 @@ package Advance_OOP;
 
 public final class ImmutableCircle {
 
-	final double radius = 8;
+	final double radius;
 	
 	ImmutableCircle(double r)
 	{
-		this.radius= r;
+		this.radius = r;
+		
 	}
 	
-	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ImmutableCircle ic = new ImmutableCircle(8);
+		
+		System.out.println("radius "+ic.radius);
+	}
 
 }
