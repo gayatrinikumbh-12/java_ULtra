930abbfc9cfd4f0546e0941307474c3f41d55f67
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-13 14:47:14 +0530

    o

---
+++ src/Advance_OOP/Bird.java
@@ -0,0 +1,15 @@
+package Advance_OOP;
+
+public abstract class Bird {
+	
+	abstract void sing();
+	
+	void eat()
+	{
+		System.out.println("Eating...");
+	}
+	
+	
+	
+
+}
---
+++ src/Advance_OOP/Sparrow.java
@@ -0,0 +1,19 @@
+package Advance_OOP;
+
+public class Sparrow extends Bird {
+
+	
+
+	@Override
+	void sing() {
+		
+		System.out.println("Singing...");
+	}
+	
+	public static void main(String[] args) {
+	      
+		
+
+	}
+
+}
