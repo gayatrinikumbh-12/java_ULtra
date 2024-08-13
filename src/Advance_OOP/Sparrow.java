d8efc030264b9692792db227022e7d55b0769ba6
gayatri.nikumbh <gayatrinikumbh12@gmail.com>
2024-08-13 19:41:37 +0530

    o

--- src/Advance_OOP/Sparrow.java
+++ src/Advance_OOP/Sparrow.java
@@ -1,6 +1,6 @@
 package Advance_OOP;
 
-public class Sparrow extends Bird {
+public class Sparrow extends Bird implements Flyable {
 
 	
 
@@ -10,11 +10,21 @@ public class Sparrow extends Bird {
 		System.out.println("Singing...");
 	}
 	
+	
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		System.out.println("Flying...");
+	}
+	
+	
 	public static void main(String[] args) {
 	      
 		Sparrow s = new Sparrow();
 		s.eat();
 		s.sing();
+		s.fly();
 
 	}
 
